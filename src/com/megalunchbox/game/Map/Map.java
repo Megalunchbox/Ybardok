c40ea16293492678f189d11a24513487fcdaf607
Megalunchbox <megalunchbox@gmail.com>
2018-08-30 15:25:50 -0500

    Update Map.java

--- src/com/megalunchbox/game/Map/Map.java
+++ src/com/megalunchbox/game/Map/Map.java
@@ -1,21 +1,46 @@
 package com.megalunchbox.game.Map;
 
 public class Map {
+    
+    private String name;
+    private String mid;
+    
+    
+    public Map(String name, String mid) {
+        this.name = name;
+        this.mid = mid;
+    }
 
     public static void loadMap() {
 
     }
 
-    public static void createMap() {
-
+    public static Map createMap(String name, String mid) {
+        Map newMap = new Map(name, mid);
+        
+        return newMap;
     }
 
     public static void copyMap() {
 
     }
 
-    public static void deleteMap() {
-
+    public static void deleteMap(String name) {
     }
-
+    
+    public static void deleteMap(Map map) {
+        
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getMid() {
+        return mid;
+    }
 }
