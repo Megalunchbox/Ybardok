4c070465dbd487b94822845ea40c035bef5ba37e
Megalunchbox <megalunchbox@gmail.com>
2018-08-31 13:29:42 -0500

    add serialversionuid to region

--- src/com/megalunchbox/game/Map/Region/Region.java
+++ src/com/megalunchbox/game/Map/Region/Region.java
@@ -8,11 +8,16 @@ public class Region implements Serializable {
   
   public Arraylist<Chunk> chunks;
   public Location location;
+  private static final long serialVersionUID 23L;
   
   public static void saveRegion(Region region) {
   }
   
   public static void delRegion(Region region) {
+  }
+  
+  public long getSerialVersionUID() {
+    return serialVersionUID;
   }
   
 }
