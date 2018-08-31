61807ac4f1fc1075915930429a21a6b8b61bde72
Megalunchbox <megalunchbox@gmail.com>
2018-08-31 13:25:13 -0500

    Update Chunk.java

--- src/com/megalunchbox/game/Map/Region/Chunk.java
+++ src/com/megalunchbox/game/Map/Region/Chunk.java
@@ -1,5 +1,27 @@
 package com.megalunchbox.game.Map.Region;
 
+import java.util.ArrayList;
+import com.megalunchbox.game.Map.Biome.Biome;
+
 public class Chunk {
+  
+  private static int chunkSize = 32;
+  private ArrayList<Tile> tiles;
+  
+  public Chunk() {
+    
+  }
+  
+  public void addTile(Tile tile) {
+    tiles.add(tile);
+  }
+  
+  public void removeTile() {
+    tiles.remove(tile);
+  }
+  
+  public void genChunk(Biome biome) {
+    
+  }
 
 }
