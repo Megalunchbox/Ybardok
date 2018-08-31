91d957e536ac84ebd7a05058eec8ab64f9f9fab5
Megalunchbox <megalunchbox@gmail.com>
2018-08-31 13:00:53 -0500

    Update Region.java

--- src/com/megalunchbox/game/Map/Region/Region.java
+++ src/com/megalunchbox/game/Map/Region/Region.java
@@ -1,3 +1,18 @@
-public class Region {
+package com.megalunchbox.game.Map.Region;
 
+import java.io.Serializable;
+import java.util.ArrayList;
+import com.megalunchbox.game.util.Location;
+
+public class Region implements Serializable {
+  
+  public Arraylist<Chunk> chunks;
+  public Location location;
+  
+  public static void saveRegion(Region region) {
+  }
+  
+  public static void delRegion(Region region) {
+  }
+  
 }
