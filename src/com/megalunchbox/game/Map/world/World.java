d251005e0f9d698914bb5ef2a7675e453fd1de87
Megalunchbox <megalunchbox@gmail.com>
2018-09-04 15:37:24 -0500

    Update World.java

--- src/com/megalunchbox/game/Map/world/World.java
+++ src/com/megalunchbox/game/Map/world/World.java
@@ -1,5 +1,12 @@
+import java.util.ArrayList;
+import com.megalunchbox.game.Map.Region.Region;
+import java.io.Serializable;
 
-public class World {
-
+public class World implements Serializable {
+  
+  private ArrayList<Region> regions;
+  private String name;
+  private final long serialVersionUUID = 41L;
+  
 
 }
