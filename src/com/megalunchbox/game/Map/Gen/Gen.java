21dafb53e66a18919c91fc2cdaa7010c51572ef7
Megalunchbox <megalunchbox@gmail.com>
2018-09-14 13:07:21 -0500

    Update Gen.java

--- src/com/megalunchbox/game/Map/Gen/Gen.java
+++ src/com/megalunchbox/game/Map/Gen/Gen.java
@@ -1,5 +1,20 @@
 package com.megalunchbox.game.map.gen;
 
+import com.megalunchbox.game.map.tile.TileSet;
+import com.megalunchbox.game.map.world.World;
+
 public class Gen {
+  
+  int xMax;
+  int yMax;
+  
+  int xMin;
+  int yMin;
+  
+  //the tileSet the map will use to gen
+  TileSet tileSet;
+  
+  public Gen(TileSet tileSet, World world) {
+  }
 
 }
