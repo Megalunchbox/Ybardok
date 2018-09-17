af52986cfbf0483e23f2ec7bd994ce3b7518ee62
Megalunchbox <megalunchbox@gmail.com>
2018-09-17 13:35:24 -0500

    Update Temperature.java

--- src/com/megalunchbox/game/Map/Region/Temperature.java
+++ src/com/megalunchbox/game/Map/Region/Temperature.java
@@ -1,5 +1,28 @@
 package com.megalunchbox.game.map.region;
 
-public class Temperature {
+public enum Temperature {
+  
+  HIGHEST(127),
+  HOT(80),
+  MEDIUM(0),
+  COLD(-80),
+  LOWEST(-127),
+  ;
+  
+  private byte temperature;
+  private String description;
+  
+  public Temperature(byte temperature) {
+    this.temperature = temperature;
+    if (temperature >= -30 || temperature <= 30) {
+      this.description = "Relatively good temperature";
+    } else if (temperature <= -30) {
+      this.description = "Cold temperature";
+    } else if (temperature >= 30){
+      this.description = "Hot temperature";
+    }
+  }
+  
+  
 
 }
