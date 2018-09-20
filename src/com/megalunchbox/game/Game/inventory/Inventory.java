c02af8acca20ea87f7b87550b82d7ab157ff7671
Megalunchbox <megalunchbox@gmail.com>
2018-09-20 13:57:03 -0500

    Update Inventory.java

--- src/com/megalunchbox/game/Game/inventory/Inventory.java
+++ src/com/megalunchbox/game/Game/inventory/Inventory.java
@@ -1,1 +1,12 @@
+package com.megalunchbox.game.game.inventory;
 
+import com.megalunchbox.game.game.item.Item;
+
+public class Inventory {
+  
+  HashMap<Integer, Item> inventory;
+  
+  public Inventory(Inventory inventory) {
+    this.inventory = inventory;
+  }
+}
