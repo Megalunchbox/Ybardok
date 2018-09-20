326e332d577a366308e52056d19aa73a50aeee69
Megalunchbox <megalunchbox@gmail.com>
2018-09-20 13:56:58 -0500

    Update Item.java

--- src/com/megalunchbox/game/Game/item/Item.java
+++ src/com/megalunchbox/game/Game/item/Item.java
@@ -1,5 +1,50 @@
 package com.megalunchbox.game.game.item.Item.java;
 
 public class Item {
+  
+  ItemType itemType;
+  
+  int durability,
+  int maxDurability;
+  
+  float damage;
+  
+  String name,
+  description;
+  
+  
+  public Item(ItemType itemType, int durability, int maxDurability, float damage, String name, String description) {
+    this.itemType = itemType;
+    this.name = name;
+    this.description = description;
+    this.damage = damage;
+    this.maxDurability = maxDurability;
+    this.durability = durability;
+  }
+  
+  public static enum ItemType {
+    
+    SWORD(1, "sword"),
+    AXE(2, "axe"),
+    ;
+    
+    int id;
+    String family;
+    
+    public ItemType(int id, String family) {
+      this.id = id;
+      this.family = family;
+    }
+    
+    public int getId() {
+      return id;
+    }
+    
+    public String getFamily() {
+      return family;
+    }
+    
+    
+  }
 
 }
