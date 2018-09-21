b81937eb6f7d0b791130ff4aa8289674698a446f
Megalunchbox <megalunchbox@gmail.com>
2018-09-21 13:37:30 -0500

    remove temperatureMap

--- src/com/megalunchbox/game/Map/Biome/Biome.java
+++ src/com/megalunchbox/game/Map/Biome/Biome.java
@@ -6,20 +6,10 @@ import java.util.HashMap;
 
 public class Biome {
 
-
-    /**
-     * @var temperatureMap should contain three string keys, high, low, and average for the biome temperatures.
-     */
-
-
-    HashMap<String, Integer> temperatureMap = new HashMap<String, Integer>();
     TileSet tileSet;
 
-    public Biome(int high, int low, int average, TileSet tileSet) {
+    public Biome(TileSet tileSet) {
 
-        temperatureMap.put("high", high);
-        temperatureMap.put("low", low);
-        temperatureMap.put("average", average);
         this.tileSet = tileSet;
 
     }
