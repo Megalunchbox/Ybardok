2c49b0827f61ce85d10d8fcec589c8050d5bd09a
Megalunchbox <megalunchbox@gmail.com>
2018-09-21 13:49:06 -0500

    Update Render.java

--- src/com/megalunchbox/game/Game/Render.java
+++ src/com/megalunchbox/game/Game/Render.java
@@ -4,10 +4,15 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Render {
 
-    public static void render(SpriteBatch batch) {
-
-
-
+    public static void render(SpriteBatch batch, OrthographicCamera cam, Mode mode, World world) {
+        if (mode == Mode.REGION_MODE) {
+        for (Region region : world.getLoadedRegions()) {
+            Color color = new Color(region.getTemperature()); //TODO: improve this... can't test yet
+            //draw biome region texture code
+            //code to only draw stuff on camera 
+            //ahahahah
+        }
+        }
     }
 
 
