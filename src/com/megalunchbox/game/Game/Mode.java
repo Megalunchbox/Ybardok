914d1586e02a3896198ed84c1492a34e92fd70b9
Megalunchbox <megalunchbox@gmail.com>
2018-09-21 13:23:11 -0500

    Update Mode.java

--- src/com/megalunchbox/game/Game/Mode.java
+++ src/com/megalunchbox/game/Game/Mode.java
@@ -1,7 +1,20 @@
 package com.megalunchbox.game.game;
 
 public enum Mode {
-
   
+  REGION_MODE(0,"Region Mode"),
+  CHUNK_MODE(0,"Chunk Mode");
 
+  int id;
+  String name;
+  
+  Mode(int id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+  
+  @Override
+  public String toString() {
+    return "id: " + id + " name: " + name;
+  }
 }
