ca079f4f267daa47f30f0d3b188bc1af07f8c8e1
Megalunchbox <megalunchbox@gmail.com>
2018-10-01 13:33:20 -0500

    Create TileContent.java

---
+++ src/com/megalunchbox/game/Map/Tile/TileContent.java
@@ -0,0 +1,15 @@
+package com.megalunchbox.game.map.tile;
+
+public class TileContent {
+
+    Texture texture;
+    Tile owner;
+    
+    public Texture texture() {
+      return texture;
+    }
+    
+    public Tile owner() {
+      return owner;
+    }
+}
