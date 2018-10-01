72dded9b947992f47678b097306d3210bacf2465
Megalunchbox <megalunchbox@gmail.com>
2018-10-01 13:40:50 -0500

    Create ChunkContent.java

---
+++ src/com/megalunchbox/game/Map/chunk/ChunkContent.java
@@ -0,0 +1,16 @@
+package com.megalunchbox.game.map.chunk;
+
+public class ChunkContent {
+  
+    Texture texture;
+    Chunk owner;
+    
+    public Texture texture() {
+      return texture;
+    }
+    
+    public Chunk owner() {
+      return owner;
+    }
+  
+}
